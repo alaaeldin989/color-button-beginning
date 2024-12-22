bfa16076f83214d7bd3a1f4434d520c88150ba5b
Alaaeldin <145039473+alaaeldin989@users.noreply.github.com>
2024-12-22 19:27:28 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+
+function red() {
+    document.getElementById("demo").style.color = "red";
+}
+function green() {
+    document.getElementById("demo").style.color = "green";
+}
+function blue() {
+    document.getElementById("demo").style.color = "blue";
+}
